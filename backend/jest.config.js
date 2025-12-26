ac30d7b2f91b55c6d79b7c938f04ea29235aa001
Joey727 <joshuasackeyjr.1124@gmail.com>
2025-12-26 14:42:20 +0000

    testing config file

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,11 +1,9 @@
-const { createDefaultPreset } = require("ts-jest");
+import { createDefaultPreset } from "ts-jest";
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.test.ts"],
-  clearMocks: true
-};
+export const preset = "ts-jest";
+export const testEnvironment = "node";
+export const testMatch = ["**/__tests__/**/*.test.ts"];
+export const clearMocks = true;
